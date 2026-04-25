84cc0e178f804d30a3c14acdb8146952bdfa6fef
C.Khoirivayanti <tsukishimaritsukei@gmail.com>
2026-04-25 23:03:49 +0700

    Implement plane ticket creation and summary printing
    Added functionality to create and display plane tickets.

--- LatihanMemberClass/Program.cs
+++ LatihanMemberClass/Program.cs
@@ -10,6 +10,22 @@ namespace LatihanMemberClass
     {
         static void Main(string[] args)
         {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = "100";
+            ticket1.Currency = "USD";
+
+            ticket2.Origin = "JKT";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = "750000";
+            ticket2.Currency = "IDR";
+
+            ticket1.PrintSummary();
+            ticket2.PrintSummary();
+            Console.ReadLine();
         }
     }
 }
